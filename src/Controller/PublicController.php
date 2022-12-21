b51d518d8aece10a65c04c61cfcf66eb39b73946
Frantz680 <dubdub68250@hotmail.fr>
2022-12-21 19:43:02 +0100

    Ajout public/html

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -5,14 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class PublicController extends AbstractController
 {
-    #[Route('/public', name: 'app_public')]
-    public function index(): Response
+    #[Route('/', name: 'public')]
+    public function public(): Response
     {
         return $this->render('public/index.html.twig', [
             'controller_name' => 'PublicController',
+        ]);
+    }
+
+    #[Route('/mentions-legales', name: 'ml')]
+    public function ml(Request $request):Response {
+
+        //On recherche toutes les catégories
+        $allCategories = $this->doctrine->getManager()->getRepository(Categorie::class)->findAll();
+
+        return $this->render('public/ml.html.twig', [
+            'allCategories' => $allCategories,
         ]);
     }
 }
